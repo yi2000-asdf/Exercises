3729467945c63fb5f9609274916ae0ebe89d3d45
yi2000 <yi2000@protonmail.com>
2020-02-22 21:23:14 +0300

    #3

--- src/Box.java
+++ src/Box.java
@@ -3,20 +3,37 @@ public class Box {
     double height;
     double depth;
 
+    double volume(){
+
+        return width*height*depth;
+
+    }
+
+
+    static class boxDemo3{
 
 
     public static void main (String args[]){
 
-        Box mybox = new Box();
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+
 
         double vol;
 
-        mybox.width =10;
-        mybox.height=20;
-        mybox.depth =15;
-        vol=mybox.width*mybox.height*mybox.depth;
+        mybox1.width =10;
+        mybox1.height=20;
+        mybox1.depth =15;
 
-        System.out.println("Volume is "+vol);
+        mybox2.width =8;
+        mybox2.height=7;
+        mybox2.depth =3;
+
+
+        double vol1 = mybox1.volume();
+        double vol2 = mybox2.volume();
+
+        System.out.println(vol2);
     }
-
+    }
 }
